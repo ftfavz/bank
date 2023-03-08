e1ea2989d96f846892cf74d12ad3eaaf8d07aebb
Muhammad Fawas CM <favasmcz@gmail.com>
2023-03-08 12:37:28 +0530

    adding files

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,64 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  data="your perfect banking partner"
+  data1="enter your acount number"
+
+  // acno:""
+  // or
+  acno:any
+  pass:any
+
+  userDetails:any={
+    1000:{username:"anu",acno:1000,password:"abc123",balance:0},
+    1001:{username:"manu",acno:1001,password:"abc123",balance:0},
+    1002:{username:"sinu",acno:1002,password:"abc123",balance:0},
+    1003:{username:"fanu",acno:1003,password:"abc123",balance:0},
+    1004:{username:"sanu",acno:1004,password:"abc123",balance:0},
+    1005:{username:"nani",acno:1005,password:"abc123",balance:0},
+    1006:{username:"neena",acno:1006,password:"abc123",balance:0}
+
+
+
+
+
+
+
+  }
+  // methods
+
+  login(){
+    var acnum=this.acno 
+    var userDetails=this.userDetails
+    var psw=this.pass
+    if(acnum in userDetails){
+      if(psw==userDetails[acnum]["password"]){
+        alert('login success')
+        
+      }
+      else{
+        alert("incorrect password")
+      }
+
+    }else{
+      alert("incorrect acnum")
+    }
+    // alert("login worked")
+  }
+  acnoChange(event:any){
+    this.acno=event.target.value
+    // console.log(this.acno);
+    
+  }
+  passChange(event:any){
+    this.pass=event.target.value
+    // console.log(this.pass);
+    
+  }
+
+}
