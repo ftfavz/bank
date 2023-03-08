a4f32459999797a3d3211d01a5506ebaa9b72ed1
Muhammad Fawas CM <favasmcz@gmail.com>
2023-03-08 14:49:57 +0530

    secondcommit

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -9,10 +9,7 @@ export class LoginComponent {
   data="your perfect banking partner"
   data1="enter your acount number"
 
-  // acno:""
-  // or
-  acno:any
-  pass:any
+
 
   userDetails:any={
     1000:{username:"anu",acno:1000,password:"abc123",balance:0},
@@ -20,8 +17,8 @@ export class LoginComponent {
     1002:{username:"sinu",acno:1002,password:"abc123",balance:0},
     1003:{username:"fanu",acno:1003,password:"abc123",balance:0},
     1004:{username:"sanu",acno:1004,password:"abc123",balance:0},
-    1005:{username:"nani",acno:1005,password:"abc123",balance:0},
-    1006:{username:"neena",acno:1006,password:"abc123",balance:0}
+    1005:{username:"munnu",acno:1005,password:"abc123",balance:0},
+    1006:{username:"linu",acno:1006,password:"abc123",balance:0}
 
 
 
@@ -32,33 +29,58 @@ export class LoginComponent {
   }
   // methods
 
-  login(){
-    var acnum=this.acno 
-    var userDetails=this.userDetails
-    var psw=this.pass
-    if(acnum in userDetails){
-      if(psw==userDetails[acnum]["password"]){
-        alert('login success')
+  // login(){
+  //   var acnum=this.acno 
+  //   var userDetails=this.userDetails
+  //   var psw=this.pass
+  //   if(acnum in userDetails){
+  //     if(psw==userDetails[acnum]["password"]){
+  //       alert('login success')
         
-      }
-      else{
-        alert("incorrect password")
-      }
+  //     }
+  //     else{
+  //       alert("incorrect password")
+  //     }
 
-    }else{
-      alert("incorrect acnum")
+  //   }else{
+  //     alert("incorrect acnum")
+  //   }
+  //   // alert("login worked")
+  // }
+  // acnoChange(event:any){
+  //   this.acno=event.target.value
+  //   // console.log(this.acno);
+    
+  // }
+  // passChange(event:any){
+  //   this.pass=event.target.value
+  //   // console.log(this.pass);
+    
+  // }
+
+// }
+
+
+login(acnum:any,psw:any){
+  console.log(acnum.value,psw.value);
+  
+  var acnum=acnum.value
+    var psw=psw.value
+  var userDetails=this.userDetails
+
+  if(acnum in userDetails){
+    if(psw==userDetails[acnum]["password"]){
+      alert('login success')
+      
     }
-    // alert("login worked")
-  }
-  acnoChange(event:any){
-    this.acno=event.target.value
-    // console.log(this.acno);
-    
-  }
-  passChange(event:any){
-    this.pass=event.target.value
-    // console.log(this.pass);
-    
-  }
+    else{
+      alert("incorrect password")
+    }
 
+  }else{
+    alert("incorrect acnum")
+  }
+  // alert("login worked")
 }
+}
+
